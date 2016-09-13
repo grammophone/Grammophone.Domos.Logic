f07f8c3c959e76c1f03a33cbbbfbd5cb205b392d
grammophone <emmanuel.liossis@gmail.com>
2016-09-13 22:38:51 +0300

    Initial definition of IWorkflowAction.

---
+++ IWorkflowAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grammophone.Domos.DataAccess;
+using Grammophone.Domos.Domain;
+using Grammophone.Domos.Domain.Workflow;
+
+namespace Grammophone.Domos.Logic
+{
+	/// <summary>
+	/// Interface to be implemented by actions invoked during
+	/// state transitions.
+	/// </summary>
+	/// <typeparam name="U">The type of the user, derived from <see cref="User"/>.</typeparam>
+	/// <typeparam name="ST">The type of the state transition, derived fom <see cref="StateTransition{U}"/>.</typeparam>
+	/// <typeparam name="D">The type of domain container, derived from <see cref="IWorkflowUsersDomainContainer{U, ST}"/>.</typeparam>
+	/// <typeparam name="S">The type of session, derived from <see cref="Session{U, D}"/>.</typeparam>
+	public interface IWorkflowAction<U, ST, D, S>
+		where U : User
+		where ST : StateTransition<U>
+		where D : IWorkflowUsersDomainContainer<U, ST>
+		where S : Session<U, D>
+	{
+		/// <summary>
+		/// Execute the action.
+		/// </summary>
+		/// <param name="session">The session.</param>
+		/// <param name="stateful">The stateful instance to execute upon.</param>
+		/// <param name="stateTransition">The state transition being executed.</param>
+		/// <param name="actionArguments">The arguments to the action.</param>
+		/// <returns>Returns a task completing the operation.</returns>
+		Task ExecuteAsync(
+			S session, 
+			IStateful<U> stateful, 
+			ST stateTransition, 
+			IDictionary<string, object> actionArguments);
+	}
+}
