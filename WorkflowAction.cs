52b4b43e19b695f5e3ce432c140848267aff4d57
grammophone <emmanuel.liossis@gmail.com>
2017-01-23 22:20:39 +0200

    Add WorkflowAction base with rights elevation capabilities.

---
+++ WorkflowAction.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grammophone.DataAccess;
+using Grammophone.Domos.DataAccess;
+using Grammophone.Domos.Domain;
+using Grammophone.Domos.Domain.Workflow;
+
+namespace Grammophone.Domos.Logic
+{
+	/// <summary>
+	/// Base for <see cref="IWorkflowAction{U, D, S, ST, SO}"/> implementations
+	/// with additional access rights elevation methods.
+	/// </summary>
+	/// <typeparam name="U">The type of the user, derived from <see cref="User"/>.</typeparam>
+	/// <typeparam name="D">The type of domain container, derived from <see cref="IWorkflowUsersDomainContainer{U, ST}"/>.</typeparam>
+	/// <typeparam name="S">The type of session, derived from <see cref="Session{U, D}"/>.</typeparam>
+	/// <typeparam name="ST">The type of state transition, derived from <see cref="StateTransition{U}"/>.</typeparam>
+	/// <typeparam name="SO">The type of stateful object, derived from <see cref="IStateful{U, ST}"/>.</typeparam>
+	public abstract class WorkflowAction<U, D, S, ST, SO> : IWorkflowAction<U, D, S, ST, SO>
+		where U : User
+		where D : IUsersDomainContainer<U>
+		where S : Session<U, D>
+		where ST : StateTransition<U>
+		where SO : IStateful<U, ST>
+	{
+		#region Public methods
+
+		/// <summary>
+		/// Execute the action.
+		/// </summary>
+		/// <param name="session">The session.</param>
+		/// <param name="domainContainer">The domain container used by the session.</param>
+		/// <param name="stateful">The stateful instance to execute upon.</param>
+		/// <param name="stateTransition">The state transition being executed.</param>
+		/// <param name="actionArguments">The arguments to the action.</param>
+		/// <returns>Returns a task completing the operation.</returns>
+		public abstract Task ExecuteAsync(
+			S session, 
+			D domainContainer, 
+			SO stateful, 
+			ST stateTransition, 
+			IDictionary<string, object> actionArguments);
+
+		/// <summary>
+		/// Get the specifications of parameters expected in the
+		/// parameters dictionary used 
+		/// by <see cref="ExecuteAsync(S, D, SO, ST, IDictionary{string, object})"/>
+		/// method.
+		/// </summary>
+		/// <returns>Returns a collection of parameter specifications.</returns>
+		public abstract IEnumerable<ParameterSpecification> GetParameterSpecifications();
+
+		#endregion
+
+		#region Protected methods
+
+		/// <summary>
+		/// Get a scope of elevated access, taking care of nesting.
+		/// Please ensure that <see cref="ElevatedAccessScope.Dispose"/> is called in all cases.
+		/// </summary>
+		/// <param name="session">The session whose rights are elevated.</param>
+		/// <remarks>
+		/// Until all nested of elevated access scopes are disposed,
+		/// no security checks are performed by the session.
+		/// </remarks>
+		protected static ElevatedAccessScope GetElevatedAccessScope(S session) 
+			=> session.GetElevatedAccessScope();
+
+		/// <summary>
+		/// Elevate access to all entities for the duration of a <paramref name="transaction"/>,
+		/// taking care of any nesting.
+		/// </summary>
+		/// <param name="session">The session whose rights are elevated.</param>
+		/// <param name="transaction">The transaction.</param>
+		/// <remarks>
+		/// This is suitable for domain containers having <see cref="TransactionMode.Deferred"/>,
+		/// where the saving takes place at the topmost transaction.
+		/// The <see cref="GetElevatedAccessScope"/> method for elevating access rights might 
+		/// restore them too soon.
+		/// </remarks>
+		protected static void ElevateTransactionAccessRights(S session, ITransaction transaction)
+			=> session.ElevateTransactionAccessRights(transaction);
+
+		#endregion
+	}
+}
