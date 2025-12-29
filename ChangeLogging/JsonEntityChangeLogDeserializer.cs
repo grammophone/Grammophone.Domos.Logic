16eb41d5b53a72fae50e39362af0fcd78448ba7b
grammophone <emmanuel.liossis@gmail.com>
2025-12-29 08:22:02 -0500

    Add the JsonEntityChangeLogDeserializer for handling complex types seralized as JSON in property states.

---
+++ ChangeLogging/JsonEntityChangeLogDeserializer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Grammophone.DataAccess;
+
+namespace Grammophone.Domos.Logic.ChangeLogging
+{
+	/// <summary>
+	/// Deserializer that resolves JSON complex objects into the respective complex type properties.
+	/// </summary>
+	/// <typeparam name="D">The type of the domain container to use for entity creation.</typeparam>
+	public class JsonEntityChangeLogDeserializer<D> : EntityChangeLogDeserializer<D>
+		where D : IDomainContainer
+	{
+		/// <inheritdoc/>
+		public JsonEntityChangeLogDeserializer(D domainContainer) : base(domainContainer)
+		{
+		}
+
+		/// <summary>
+		/// If the <paramref name="value"/> is of type <see cref="JsonElement"/>, attempt to deserialize it in the type specified by <paramref name="propertyInfo"/>
+		/// and set it in the <paramref name="entity"/>.
+		/// </summary>
+		protected override void ResolvePropertyAssignment<E>(E entity, PropertyInfo propertyInfo, object value)
+		{
+			if (value is JsonElement jsonElement)
+			{
+				var deserializedValue = jsonElement.Deserialize(propertyInfo.PropertyType);
+
+				if (deserializedValue != null)
+				{
+					propertyInfo.SetValue(entity, deserializedValue);
+				}
+			}
+		}
+	}
+}
